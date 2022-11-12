67b317aa4b4cdd9353da2ea585573b6ad757ce25
Rizqi Augustian <86402914+rizqinura99@users.noreply.github.com>
2022-11-12 15:40:21 +0700

    Add files via upload

---
+++ Source Code/Android_Application_For_Multi-Sensor_Data_Monitoring/Android_Application_For_Multi-Sensor_Data_Monitoring/Temp_multi_sensor/Temp_multi_sensor.ino
@@ -0,0 +1,16 @@
+float pin1=A1;
+float value1=0;
+float pin2=A2;
+float value2=0;
+void setup() { 
+  Serial.begin(9600);
+}
+void loop() {
+value1=analogRead(pin1);
+Serial.println((value1*500)/1023);
+Serial.println("|");
+value2=analogRead(pin2);
+Serial.println((value2*500)/1023);
+delay(1000);
+
+}
